d12ec12885142be2777ceda8cc76fc7ddda13911
Murilo Stuart <stuartmurilo10@gmail.com>
2024-05-02 21:15:54 -0300

    att

--- java-anatomia-classes/src/MinhaClasse2.java
+++ java-anatomia-classes/src/MinhaClasse2.java
@@ -1,6 +1,6 @@
 public class MinhaClasse2 {
     public static void main(String[] args) {
-        System.out.println("Oi foi executado pelo terminal");
+        System.out.println("Oi fui executado pelo terminal");
     }
     
 }
