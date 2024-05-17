e7c671603d411b7b5670c794bbecf6c3bfa9d32c
Murilo Stuart <stuartmurilo10@gmail.com>
2024-05-16 21:19:52 -0300

    att

---
+++ controle-de-fluxo-desafio/src/Contador.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Contador {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int num1 = scanner.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int num2 = scanner.nextInt();
+
+        try {
+            contar (num1,num2);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+    }
+
+
+    static void contar(int num1, int num2) throws ParametrosInvalidosException {
+    //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+    
+    int contagem = parametroDois - parametroUm;
+    //realizar o for para imprimir os números com base na variável contagem
+  }  
+}
