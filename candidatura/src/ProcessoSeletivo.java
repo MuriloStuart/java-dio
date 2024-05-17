d6c3cec1ddf411a4269725336c90975c1abf36ff
Murilo Stuart <stuartmurilo10@gmail.com>
2024-05-16 21:01:58 -0300

    att

--- candidatura/src/ProcessoSeletivo.java
+++ candidatura/src/ProcessoSeletivo.java
@@ -1,10 +1,43 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 
     public static void main(String[] args) {
-       imprimirSelecionados();
+        String [] candidatos = {"Felipe," , "Marcia" , "Julia" , "Paulo" , "Augusto"};
+        for(String candidato: candidatos){
+            entrandoEmContato(candidato);
+
+        }
         
+    }
+
+    static void entrandoEmContato(String candidato) {
+         int tentativasRealizadas = 1;
+         boolean continuarTentando = true;
+         boolean atendeu = false;
+         do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando)
+               tentativasRealizadas++;
+            else
+               System.out.println("Contato realizado com sucesso");
+
+
+         }while(continuarTentando && tentativasRealizadas <3);
+
+
+         if(atendeu)
+           System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + " tentativa");
+        else
+           System.out.println("Não conseguimos contato com " + candidato + ", número maximo de tentativas realizadas " + tentativasRealizadas);
+
+         
+    }
+
+    static boolean atender() {
+        return new Random().nextInt(3)==1;
     }
 
     static void imprimirSelecionados(){
@@ -16,7 +49,7 @@ public class ProcessoSeletivo {
 
         }
 
-        System.out.println("Forme abreviada de interação for each");
+        System.out.println("Forma abreviada de interação for each");
 
         for(String candidato: candidatos){
             System.out.println("O candidato selecionado foi " + candidato);
